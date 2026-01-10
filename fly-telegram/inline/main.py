7b7a1df4cccadb094efec0c4e89807add56b0595
HotDrify <105966726+HotDrify@users.noreply.github.com>
2026-01-10 20:35:43 +0300

    debug

--- fly-telegram/inline/main.py
+++ fly-telegram/inline/main.py
@@ -87,6 +87,7 @@ class Inline:
     async def start(self):
         self.register_handlers(self.dp)
         logging.info("Inline bot is loaded.")
+        logging.debug("dict object: %s", self.bot.handlers)
         asyncio.ensure_future(self.dp.start_polling())
 
     async def stop(self):
